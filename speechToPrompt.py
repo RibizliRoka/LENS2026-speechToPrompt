29572acaef734d5fd065fe2571a5043239ce9c70
RibizliRoka <lauraszabo777@gmail.com>
2026-04-12 18:25:08 -0500

    first commit

---
+++ speechToPrompt.py
@@ -0,0 +1,19 @@
+
+
+# I was thinking of using this to do the speech to text:
+# https://realpython.com/python-speech-recognition/
+
+# Once we get the text we can give it to gemini or something to make it into an actual
+# text we can use with the model
+
+# Also some potential cool factor ideas:
+
+# -> Sticks - we can make a sort of sticks game with different colored blocks that are worth different
+# amounts of points and we'd always ask it to pick up the next most valuable block and
+# play against a person
+
+# -> Recipie - robot picks up tiny food items which are ingredients to make something
+
+# -> Organize by Color - robot picks up like colored tapes or blocks and puts them in
+# different bins based on color, or this could be done with types of food, animals
+# plants, etc 
